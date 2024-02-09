e7ac29f703c3d02b3d05e5b886520defd36382a3
Uriel G. Rojo <ug.rojo92@gmail.com>
2024-02-09 15:32:32 -0600

    Initial barebones Spring Boot service

---
+++ src/main/java/com/moodic/moodic/MoodicApplication.java
@@ -0,0 +1,13 @@
+package com.moodic.moodic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moodic/moodic/MoodicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodic.moodic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
